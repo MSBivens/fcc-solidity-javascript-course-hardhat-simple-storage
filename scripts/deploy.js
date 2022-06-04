7ff15e95adfe146260532f828445f04fb9bdbce3
MSBivens <mikebiv95@gmail.com>
2022-06-04 17:09:42 -0500

    worked on main

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,20 @@
 // imports
+const { solidity } = require("ethereum-waffle")
+const { ethers } = require("hardhat")
 
 // async main
-async function main() {}
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed()
+  console.log(`Deployed contract to: ${simpleStorage.address}`)
+}
 
 // main
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
